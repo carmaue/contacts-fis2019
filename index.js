6aa2331a16ceb5326cc3fc29f0d6e37a81197632
root <carmaue@hotmail.com>
2019-11-29 03:13:58 +0000

    Added support to nedb for post

--- index.js
+++ index.js
@@ -2,19 +2,21 @@
 //console.log(msg);
 var express = require('express');
 var bodyParser = require('body-parser');
+var DataStore = require('nedb');
 
 var port = 3000;
 var BASE_API_PATH = "/api/v1";
-
-var contacts = [
-    {"name" : "peter","phone" : "12456"},
-    {"name" : "jhon" , "phone": "6666"}
-];
+var DB_FILE_NAME =__dirname + "/contacts.json";
 
 console.log("Starting API server..");
 
 var app = express();
 app.use(bodyParser.json());
+
+var db = new DataStore({
+    filename: DB_FILE_NAME,
+    autoload: true
+});
 
 app.get("/", (req,res) =>{
     res.send("<html><body><h1>My server</h1></body></html>");
@@ -22,15 +24,21 @@ app.get("/", (req,res) =>{
 
 app.get(BASE_API_PATH + "/contacts",(req,res)=>{
     console.log(Date() + " - GET /contacts");
-    res.send(contacts);
+    res.send([]);
 });
 
 app.post(BASE_API_PATH + "/contacts", (req,res)=>{
     console.log(Date() + " - POST /contacts");
     var contact =  req.body;
-    contacts.push(contact);
-    res.sendStatus(201);
-})
+    db.insert(contact, (err) => {
+        if(err){
+            console.log(Date() + " - "+ err);
+            res.sendStatus(500);
+        }else{
+            res.sendStatus(201)
+        }
+    });
+});
 app.listen(port);
 
 console.log("Server ready!");
